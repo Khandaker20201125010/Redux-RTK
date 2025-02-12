54abc9751e9b0730d6d5bce8171fb8e8c9f41957
unknown <prantokih42@gmail.com>
2025-02-12 23:49:52 +0600

    proiver added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={routes}></RouterProvider>
+  <Provider store={store}>
+  <RouterProvider router={routes}></RouterProvider>
+  </Provider>
   </StrictMode>,
 )
